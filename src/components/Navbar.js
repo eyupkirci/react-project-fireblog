6567d57442028cece6fe948e5b48233ec0d823a7
eyupkirci <eyupkirci@gmail.com>
2021-09-18 12:13:38 +0300

    Navbar created.

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <div className="navbar" style={{height:"75px", color:"gray", justifyContent:"space-between", backgroundColor:"aquamarine"}}>
+            <p>Navbar</p>    
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
