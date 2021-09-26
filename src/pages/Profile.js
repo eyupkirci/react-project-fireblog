1956c23a05502c9febc30428336dbb28bc404373
eyupkirci <61272579+eyupkirci@users.noreply.github.com>
2021-09-27 01:15:33 +0300

    profile page created

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { continueWithGoogle, logIn } from './../helpers/firebase';
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import { Redirect } from "react-router";
+
 
 export default function Profile() {
-    return (
-        <div>
-            Profile
+
+  const { currentUser } = useContext(AuthContext);
+  
+  
+  if (currentUser) {
+      
+      return (
+        <div className="register">
+          
+          <div className="register-form">
+            <h1 className="form-title display-3">PROFILE</h1>
+            <form id="register">
+    
+            <div className="mb-3" style={{textAlign: 'center'}}>
+                <p>{currentUser.email}</p>
+            </div>
+
+            </form>
+    
+          </div>
+          
         </div>
-    )
-}
+      );
+      
+    }
+    return <Redirect to="/" />;
+
+};
+
